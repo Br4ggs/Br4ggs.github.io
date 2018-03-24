a893bb32b3753268c111fef436d907dc0b29dad2
emiel <emielvdbrink@gmail.com>
2018-03-24 23:50:58 +0100

    added mouseover highlight and corrected some links

---
+++ js/behaviour.js
@@ -0,0 +1,38 @@
+var elem;
+var msnry;
+
+window.onload = function () {
+
+    //gets all elements of grid and grid-item and turns them into a masonry collage
+    elem = document.querySelector('.grid');
+    msnry = new Masonry(elem, {
+        itemSelector: '.grid-item',
+        gutter: 15,
+        isFitWidth: true
+    });
+
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                }
+            }
+        });
+};
